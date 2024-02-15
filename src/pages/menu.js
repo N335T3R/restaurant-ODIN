cb762155b2f8f33b080ae81189f3a10dbed2da7b
n335t3r <n335t3r@gmail.com>
2024-02-15 15:02:57 -0500

    drink names & prices created as 2 uls opposing each other in div beneath drink header;

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -11,10 +11,17 @@ export default function displayMenu() {
     content.appendChild(menu);
 
 
+
+
     // DRINKS
+    const drinkMenu = document.createElement('div');
+    drinkMenu.classList.add('drinkMenu');
+    menu.appendChild(drinkMenu);
+
+    // DRINKS HEAD
     const drinksHead = document.createElement('div');
     drinksHead.classList.add('drinksHead');
-    menu.appendChild(drinksHead);
+    drinkMenu.appendChild(drinksHead);
 
     const drinks = document.createElement('h1');
     drinks.classList.add('drinks');
@@ -25,5 +32,31 @@ export default function displayMenu() {
     drinkIcon.src = Martini;
     drinkIcon.classList.add('drinkIcon');
     drinksHead.appendChild(drinkIcon);
+    // END DRINKS HEAD
+
+
+    // DRINKS CONTENT
+    const drinksContent = document.createElement('div');
+    drinksContent.classList.add('drinksContent');
+    drinkMenu.appendChild(drinksContent);
+
+    // drink names
+    const drinkList = document.createElement('ul');
+    drinkList.classList.add('drinkList');
+    drinksContent.appendChild(drinkList);
+
+    const martini = document.createElement('li');
+    martini.textContent = "martini";
+    drinkList.appendChild(martini);
+
+
+    // drinkPrices
+    const priceList = document.createElement('ul');
+    priceList.classList.add('priceList');
+    drinksContent.appendChild(priceList);
+
+    const martiniP = document.createElement('li');
+    martiniP.textContent = "$7";
+    priceList.appendChild(martiniP);
 };
 
